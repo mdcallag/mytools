507f9a9e7ea321a5752eba871014e01fb487dace
mark <mdcallag@mdcallag-laptop>
2009-04-18 08:35:27 -0700

    add run_fileio, run_ib

---
+++ bench/run_fileio/run.sh
@@ -0,0 +1,43 @@
+# max value for --num-threads
+maxdop=$1
+
+file_num=$2
+file_total_size=$3
+
+# rndrd rndwr rndrw seqrd seqwr
+file_test_mode=$4
+
+# direct, ""
+file_extra_flags=$5
+
+max_time=$6
+
+sbargs="--test=fileio --file-num=$file_num --file-total-size=$file_total_size \
+        --file-test-mode=$file_test_mode --file-extra-flags=$file_extra_flags"
+
+rm -f test_file.*
+
+rm -f fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+rm -f fi.r.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+touch fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+
+dop=1
+while [[ $dop -le $maxdop ]]; do
+  echo prepare $dop
+  echo "sysbench2 $sbargs prepare" >> \
+      fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+  ./sysbench2 $sbargs prepare >> \
+      fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+
+  echo run $dop
+  echo "sysbench2 $sbargs --num-threads=$dop --max-time=$max_time run" >> \
+      fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+  ./sysbench2 $sbargs --num-threads=$dop --max-time=$max_time run >> \
+      fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+  dop=$(( $dop * 2 ))
+done
+
+grep "Requests/sec executed" fi.o.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags} | \
+   awk '{ printf "%s ", $1 }' >> \
+       fi.r.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
+echo >> fi.r.fn_${file_num}.fts_${file_total_size}.ftm_${file_test_mode}.fef_${file_extra_flags}
---
+++ bench/run_ib/run1.sh
@@ -0,0 +1,54 @@
+nr=$1
+engine=$2
+mysql=$3
+maxdop=$4
+myu=$5
+myp=$6
+myd=$7
+tn=$8
+mysock=$9
+tag=${10}
+
+if [[ ${11} == "yes" ]]; then
+  setup="--setup"
+else
+  setup=""
+fi
+
+if [[ ${12} == "yes" ]]; then
+  insert_only="--insert_only"
+else
+  insert_only=""
+fi
+
+run_mysql="$mysql -u$myu -p$myp -S$mysock $myd -A"
+
+dop=1
+rm -f ${tag}.*
+TIMEFORMAT='%R'
+while [[ $dop -le $maxdop ]]; do
+
+  # Perform the load
+  i=1
+  while [[ $i -le $dop ]]; do
+    if [[ ${11} == "yes" ]]; then
+      $run_mysql -e "drop table if exists $tn$i"
+    fi
+    ../ibench/iibench.py --db_name=$myd --db_user=$myu --db_password=$myp --db_sock=$mysock \
+        $setup $insert_only --max_rows=$nr --rows_per_report=10000 --table_name=$tn$i \
+        >& ${tag}.${i}_of_${dop} &
+    p[$i]=$!
+    i=$(( $i + 1))
+  done
+  i=1
+  while [[ $i -le $dop ]]; do
+    wait ${p[ $i ]}
+    i=$(( $i + 1))
+  done
+  grep "^$nr " ${tag}.*_of_${dop} | awk '{ print $3 }' | sort -n | tail -1 | awk '{ print "maxloadtime", $1 }'
+  $run_mysql -e "show table status like 'tn%'"
+
+  dop=$(( $dop * 2 ))
+
+done
+
