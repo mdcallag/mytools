7bec025f9ef69477f8768eac47c586a4020c7718
Mark Callaghan <mdcallag@gmail.com>
2011-02-04 07:48:36 -0800

    add more tests

--- bench/innosim/run_all.sh
+++ bench/innosim/run_all.sh
@@ -4,29 +4,20 @@
 prepare=$1
 secs=$2
 
-# read-only 0-concur and prepare files
-bash run_innosim.sh  1 1 8  1   0   0 $secs $prepare
+if [ $prepare -gt 0 ] ; then
+echo Prepare
+bash run_innosim.sh  1 1 8  1   0   0 10 1
+fi
 
-# read-only low-concur and prepare files
-bash run_innosim.sh  1 1 8  4   0   0 $secs 0
+for concur in 1 4 32 128 ; do
 
-# read-only high-concur
-bash run_innosim.sh  1 1 8 32   0   0 $secs 0 
+echo read-only $concur concur
+bash run_innosim.sh  1 1 8  $concur   0   0 $secs 0
 
-# write-only 0-concur
-bash run_innosim.sh  1 1 8  1 100 100 $secs 0
+echo write-only $concur concur
+bash run_innosim.sh  1 1 8  $concur 100 100 $secs 0
 
-# write-only low-concur
-bash run_innosim.sh  1 1 8  4 100 100 $secs 0
+echo read-write $concur concur
+bash run_innosim.sh  1 1 8  $concur  50   0 $secs 0
 
-# write-only high-concur
-bash run_innosim.sh  1 1 8 32 100 100 $secs 0
-
-# read-write 0-concur
-bash run_innosim.sh  1 1 8  1  50   0 $secs 0
-
-# read-write low-concur
-bash run_innosim.sh  1 1 8  4  50   0 $secs 0
-
-# read-write high-concur
-bash run_innosim.sh  1 1 8 32  50   0 $secs 0
+done
