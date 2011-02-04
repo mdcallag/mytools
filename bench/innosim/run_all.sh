8678ba9b3114e0cd5d6bc3a6452223c2110062c1
Mark Callaghan <mdcallag@gmail.com>
2011-02-04 07:08:13 -0800

    add prepare, duration args to run_all

--- bench/innosim/run_all.sh
+++ bench/innosim/run_all.sh
@@ -2,21 +2,31 @@
 # binlog&trxlog doublewrite #writers #users dirty_pct read_hit_pct test_duration prepare
 
 prepare=$1
+secs=$2
 
-# read-only low-concur and optionally prepare files
-bash run_innosim.sh  1 1 8  4   0   0 300 $prepare
+# read-only 0-concur and prepare files
+bash run_innosim.sh  1 1 8  1   0   0 $secs $prepare
+
+# read-only low-concur and prepare files
+bash run_innosim.sh  1 1 8  4   0   0 $secs 0
 
 # read-only high-concur
-bash run_innosim.sh  1 1 8 32   0   0 300 0 
+bash run_innosim.sh  1 1 8 32   0   0 $secs 0 
+
+# write-only 0-concur
+bash run_innosim.sh  1 1 8  1 100 100 $secs 0
 
 # write-only low-concur
-bash run_innosim.sh  1 1 8  4 100 100 300 0
+bash run_innosim.sh  1 1 8  4 100 100 $secs 0
 
 # write-only high-concur
-bash run_innosim.sh  1 1 8 32 100 100 300 0
+bash run_innosim.sh  1 1 8 32 100 100 $secs 0
+
+# read-write 0-concur
+bash run_innosim.sh  1 1 8  1  50   0 $secs 0
 
 # read-write low-concur
-bash run_innosim.sh  1 1 8  4  50   0 300 0
+bash run_innosim.sh  1 1 8  4  50   0 $secs 0
 
 # read-write high-concur
-bash run_innosim.sh  1 1 8 32  50   0 300 0
+bash run_innosim.sh  1 1 8 32  50   0 $secs 0
