4c648922c13573823634510eef5486e533c6136e
Mark Callaghan <mdcallag@gmail.com>
2011-02-08 13:47:56 -0800

    change read-write tests to use dirty-pct=25,13,6 changed concurrency to run at --num-users=1,4,8,16,32,128

--- bench/innosim/run_all.sh
+++ bench/innosim/run_all.sh
@@ -8,16 +8,22 @@ if [ $prepare -gt 0 ] ; then
 echo Prepare
 bash run_innosim.sh  1 1 8  1   0   0 10 1
 fi
-
-for concur in 1 4 32 128 ; do
+ 
+for concur in 1 4 8 16 32 128 ; do
 
 echo read-only $concur concur
-bash run_innosim.sh  1 1 8  $concur   0   0 $secs 0
+bash run_innosim.sh  1 1 8  $concur   0   0 $secs 0; sleep $sleep_secs
 
 echo write-only $concur concur
-bash run_innosim.sh  1 1 8  $concur 100 100 $secs 0
+bash run_innosim.sh  1 1 8  $concur 100 100 $secs 0; sleep $sleep_secs
 
-echo read-write $concur concur
-bash run_innosim.sh  1 1 8  $concur  50   0 $secs 0
+echo read-write $concur concur dirty=25
+bash run_innosim.sh  1 1 8  $concur  25   0 $secs 0; sleep $sleep_secs
+
+echo read-write $concur concur dirty=12
+bash run_innosim.sh  1 1 8  $concur  12   0 $secs 0; sleep $sleep_secs
+
+echo read-write $concur concur dirty=6
+bash run_innosim.sh  1 1 8  $concur  6   0 $secs 0; sleep $sleep_secs
 
 done
