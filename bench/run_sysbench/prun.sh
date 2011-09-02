ba64ba71354bba1d2ea609550ca978df903c3a65
Mark Callaghan <mdcallag@gmail.com>
2011-09-02 09:00:40 -0700

    updates

--- bench/run_sysbench/prun.sh
+++ bench/run_sysbench/prun.sh
@@ -61,10 +61,17 @@ warmup=${18}
 # value for --oltp-range-size
 range=${19}
 
-shift 19
+nclients=${20}
+
+use_compress=${21}
+
+shift 21
+
+echo use $nclients clients and $nr rows and $t secs
 
 while (( "$#" )) ; do
   b=$1
+  echo use binary $b
   shift 1
   mybase=$ibase/$b
   mysock=$mybase/var/mysql.sock
@@ -79,29 +86,41 @@ while (( "$#" )) ; do
   ssh $dbh "$mybase/bin/mysqladmin -u$myu -p$myp -S$mysock shutdown"
 
   if [[ $runasroot == "yes" ]] ; then
-    echo ssh $dbh "$mybase/bin/mysqld_safe --user=root /dev/null 2>&1 &"
+    echo ssh $dbh "$mybase/bin/mysqld_safe --user=root > /dev/null 2>&1 &"
     ssh $dbh "$mybase/bin/mysqld_safe --user=root > /dev/null 2>&1 &"
   else
     echo ssh start
     ssh $dbh "$mybase/bin/mysqld_safe > /dev/null 2>&1 &"
   fi
+  sleep 1 
+  ssh $dbh "ls -l $mysock"
   echo Sleep after startup  
-  sleep 20
+  sleep 40
 
-  echo reset master
-  $run_mysql -B -e 'reset master'
-
-  echo ssh $dbh "$mybase/bin/mysql -u$myu -p$myp -S$mysock -e \"grant all on *.* to root@'%' identified by '$myp' with grant option \" "
-  ssh $dbh "$mybase/bin/mysql -u$myu -p$myp -S$mysock -e \"grant all on *.* to root@'%' identified by '$myp' with grant option \" "
+  echo ssh $dbh "$mybase/bin/mysql -u$myu -p$myp -S$mysock -e \"grant all on *.* to root@'%' identified by '$myp' \" "
+  ssh $dbh "$mybase/bin/mysql -u$myu -p$myp -S$mysock -e \"grant all on *.* to root@'%' identified by '$myp' \" "
   ssh $dbh "$mybase/bin/mysql -u$myu -p$myp -S$mysock mysql -e \"delete from user where length(Password) = 0; flush privileges;\""
 
-  echo Running $b $engine
-  rm -f startme
-  bash run1.sh $engine $t $nr $strx $etrx $mysql $maxdop $prepare $myu $myp $myd $dbh $usepk $dist $warmup $range > \
-      sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist &
-  fid=$!
+  rm -f ready.* go.*
 
-  while [[ ! -f startme ]]; do echo "wait for startme"; sleep 5; done
+  for c in $( seq 1 $nclients ); do
+    echo Running $b $engine for client $c of $nclients
+    rm -f startme.$c
+    echo bash prun1.sh $engine $t $nr $strx $etrx $mysql $maxdop $prepare $myu $myp $myd $dbh $usepk $dist $warmup $range $c $nclients $mybase $use_compress
+    bash prun1.sh $engine $t $nr $strx $etrx $mysql $maxdop $prepare $myu $myp $myd $dbh $usepk $dist $warmup $range $c $nclients $mybase $use_compress > \
+        sb.$c.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist &
+    fid[${c}]=$!
+    echo client $c has pid ${fid[${c}]}
+  done
+
+  for c in $( seq 1 $nclients ); do
+    while [[ ! -f startme.$c ]]; do echo "wait for startme.$c"; sleep 5; done
+  done
+
+  rm -f startme.*
+
+  # for i in $( seq 0 18 ) ; do $mybase/bin/mysql -u$myu -p$myp -h$dbh -e "show engine innodb status\G"; sleep 60 ; done > \
+  #      sb.sis.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist &
 
   if [[ $use_oprofile == "yes" ]]; then
     ssh $dbh which opcontrol
@@ -127,25 +146,42 @@ while (( "$#" )) ; do
   ssh $dbh "ps aux | grep mstat\.py | grep -v grep | awk '{ print \$2 }' | xargs kill -9"
   ssh $dbh "sysctl dev.flashcache.zero_stats=1"
   ssh $dbh "cat /proc/flashcache_stats" > sb.fc0.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist 
-  ssh $dbh "python /data/bench/mstat.py --loops 1000000 --interval 10 --db_user=$myu --db_password=$myp --db_host=$dbh" \
+  ssh $dbh "python /data/mstat.py --loops 1000000 --interval 10 --db_user=$myu --db_password=$myp --db_host=$dbh" \
                  > sb.mstat.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist &
  
-  wait $fid
+  for c in $( seq 1 $nclients ); do
+    echo wait for client $c with pid ${fid[${c}]}
+    ps aux | grep ${fid[${c}]} | grep -v grep
+    wait ${fid[${c}]}
+  done
+  echo Done waiting
 
+  rm -f /tmp/pres; touch /tmp/pres
   echo -n $b "$engine " > sb.r.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  grep transactions: sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | awk '{ print $4 }' | tr '(' ' ' > res
-  awk '{ printf "%s ", $1 }' res >> sb.r.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  for c in $( seq 1 $nclients ); do
+    grep transactions: sb.$c.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | awk '{ print $4 }' | tr '(' ' ' | awk '{ printf "%s \n", $1 }' > /tmp/res.$c
+    paste /tmp/pres /tmp/res.$c > /tmp/pres.2; mv /tmp/pres.2 /tmp/pres
+  done
+  awk '{ s=0; for (i=1; i <= NF; i += 1) { s += $i }; printf "%s ", s }' /tmp/pres >> sb.r.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
   echo >> sb.r.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
+  rm -f /tmp/pres; touch /tmp/pres
   echo -n $b "$engine " > sb.avg.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  grep avg: sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | grep -v percent | awk '{ print $3 }' | tr 'ms' ' ' | awk '{ printf "%s \n", $1 }' > res
-  awk '{ printf "%s ", $1 }' res >> sb.avg.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  for c in $( seq 1 $nclients ); do
+    grep avg: sb.$c.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | grep -v percent | awk '{ print $3 }' | tr 'ms' ' ' | awk '{ printf "%s \n", $1 }' > /tmp/res.$c
+    paste /tmp/pres /tmp/res.$c > /tmp/pres.2; mv /tmp/pres.2 /tmp/pres
+  done
+  awk '{ x=0; for (i=1; i <= NF; i += 1) { if ($1 > x) { x = $1 } }; printf "%s ", x }' /tmp/pres >> sb.avg.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
   echo >> sb.avg.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
-  echo -n $b "$engine " > sb.p95.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  grep "95 percentile:" sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | awk '{ print $5 }' | tr 'ms' ' ' | awk '{ printf "%s \n", $1 }' > res
-  awk '{ printf "%s ", $1 }' res >> sb.p95.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  echo >> sb.p95.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  rm -f /tmp/pres; touch /tmp/pres
+  echo -n $b "$engine " > sb.p99.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  for c in $( seq 1 $nclients ); do
+    grep percentile: sb.$c.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | grep approx | awk '{ print $5 }' | tr 'ms' ' ' | awk '{ printf "%s \n", $1 }' > /tmp/res.$c
+    paste /tmp/pres /tmp/res.$c > /tmp/pres.2; mv /tmp/pres.2 /tmp/pres
+  done
+  awk '{ x=0; for (i=1; i <= NF; i += 1) { if ($1 > x) { x = $1 } }; printf "%s ", x }' /tmp/pres >> sb.p99.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  echo >> sb.p99.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
   ssh $dbh killall vmstat
   ssh $dbh killall iostat
@@ -153,6 +189,7 @@ while (( "$#" )) ; do
   ssh $dbh "ps aux | grep mstat\.py | grep -v grep | awk '{ print \$2 }' | xargs kill -9"
 
   if [[ $use_oprofile == "yes" ]]; then
+    echo Get oprofile data
     ssh $dbh opcontrol --dump
     sleep 5
     ssh $dbh opreport --demangle=smart --symbols > sb.p.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
@@ -162,31 +199,32 @@ while (( "$#" )) ; do
   #
   # Innodb mutex stats
   #
-  $run_mysql -e 'show mutex status' > sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  echo Get stats
+  $run_mysql -B -e 'show engine innodb mutex' > sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
-  if grep Line sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist > /dev/null; then
-    grep -v Line sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
-        awk '{ printf "%s:%s %s\n", $1, $2, $3 }' > sb.mst.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-    mv sb.mst.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  fi
+  cat sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
+    grep -v Status | \
+    tr '=' ' ' | \
+    awk '{ printf "%10d\t%s\n", $4, $2 }' | \
+    sort -rnk 2 > \
+    sb.msn.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist 
 
   # By mutex
-  sort -k 1,1 sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
-    grep -v OS_waits | \
-    awk '{ if ($1 != pk) { if (s > 0) { printf "%10d\t%s\n", s, pk }; s = $2; pk = $1 } else { s += $2 } } END { if (s > 0) { printf "%10d\t%s\n", s, pk } } ' | \
+  sort -k 2,2 sb.msn.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
+    awk '{ if ($2 != pk) { if (s > 0) { printf "%10d\t%s\n", s, pk }; s = $1; pk = $2 } else { s += $1 } } END { if (s > 0) { printf "%10d\t%s\n", s, pk } } ' | \
     sort -r -n -k 1,1 | \
-    head -20 > sb.ms20.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+    head -50 > sb.msa.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
   # By callers
-  sort -k 1,1 sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
-    grep -v OS_waits | \
-    awk '{ if ($1 != pk) { if (s < 0) { printf "%10d\t%s\n", -s, pk }; s = $2; pk = $1 } else { s += $2 } } END { if (s < 0) { printf "%10d\t%s\n", -s, pk } } '  | \
-    sort -r -n -k 1,1 | \
-    head -20 > sb.cms20.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
+  # sort -k 1,1 sb.ms.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist | \
+  #  awk '{ if ($1 != pk) { if (s < 0) { printf "%10d\t%s\n", -s, pk }; s = $2; pk = $1 } else { s += $2 } } END { if (s < 0) { printf "%10d\t%s\n", -s, pk } } '  | \
+  #  sort -r -n -k 1,1 | \
+  #  head -20 > sb.cms20.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
   #
   # General mutex stats
   #
+  echo Get global mutex stats
   $run_mysql -e 'show global mutex status' > sb.gs.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
   # By mutex
   $run_mysql -B -e 'show global mutex status' | head -1 > sb.gs20.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
@@ -198,18 +236,21 @@ while (( "$#" )) ; do
     sort -r -n -k 3,3 | \
     head -20 > sb.cgs20.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
-  ssh $dbh "du -hs $mybase/var/test/*.ibd" >> sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-
-  $run_mysql $myd -e 'select * from sbtest where id=1' >> sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  if [ $nr -le 10000000 ]; then
-    $run_mysql $myd -e 'select min(c), max(c), avg(c), count(c), sum(c) from sbtest' >> sb.o.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
-  fi
+  $run_mysql -e 'select * from information_schema.user_statistics\G' > sb.us.$engine.$b.t_$t.r_$nr.tx_$strx.pk_$usepk.dist_$dist
 
   if [[ $drop != "no" ]]; then
-    $run_mysql $myd -e "drop table sbtest"
+    echo Drop tables
+    for c in $( seq 1 $nclients ); do
+      $run_mysql $myd -e "drop table sbtest${id}"
+    done
   fi
- 
-  echo Running $b shutdown
+
+  echo Running $b shutdown at $( date )
+  ssh $dbh "ls -l $mysock"
   ssh $dbh "$mybase/bin/mysqladmin -u$myu -p$myp -S$mysock shutdown"
+  echo Shutdown done
   sleep 5
+  echo Sleep done
 done
+
+echo Goodbye
