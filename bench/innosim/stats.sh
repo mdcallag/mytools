b673349b30dea0f00a21bc00c1b13da63d78dc5f
Mark Callaghan <mdcallag@gmail.com>
2015-08-24 11:07:24 -0700

    add iostat metrics to stats output

--- bench/innosim/stats.sh
+++ bench/innosim/stats.sh
@@ -9,7 +9,10 @@ use_dblw=$3
 # multiplier for max_concur, 2, 4, 8 etc
 concur_mult=$4
 
-for d in 0 6 17 25 100 ; do rm -f dr.$d dw.$d p.$d percentile_iops.$d report.$d ; done
+# name of storage device in iostat output
+dev=$5
+
+for d in 0 6 17 25 100 ; do rm -f dr.$d dw.$d p.$d percentile_iops.$d report.$d io.$d ; done
 rm -f r 
 
 u=1
@@ -18,19 +21,24 @@ for d in 0 6 17 25 100 ; do
 
 suffix=o.bl_${use_bltl}.trx_${use_bltl}.dblwr_${use_dblw}.wthr_8.uthr_${u}.dirty_$d
 
-grep final ${suffix}* | grep read: | awk '{ printf   "%.0f\t%s\t%s\t%s\t%s\t%s\t%s\t\n", $5, $7, $9, $11, $13, $15, $17 }' >> dr.$d 
-grep final ${suffix}* | grep ^write: | awk '{ printf "%.0f\t%s\t%s\t%s\t%s\t%s\t%s\t\n", $5, $7, $9, $11, $13, $15, $17 }' >> dw.$d 
+grep final ${suffix}* | grep read: | awk '{ printf   "%.0f\t%s\t%s\t%s\t%s\t%s\t%.1f\t\n", $5, $7, $9, $11, $13, $15, $17 }' >> dr.$d 
+grep final ${suffix}* | grep ^write: | awk '{ printf "%.0f\t%s\t%s\t%s\t%s\t%s\t%.1f\t\n", $5, $7, $9, $11, $13, $15, $17 }' >> dw.$d 
+
+iosuffix=i.bl_${use_bltl}.trx_${use_bltl}.dblwr_${use_dblw}.wthr_8.uthr_${u}.dirty_$d
+grep $dev ${iosuffix}* |  awk '{ c += 1; rs += $4; ws += $5 ; rkb += $6; wkb += $7 } END { printf "%.0f\t%.0f\t%.0f\t%.0f\n", rs/c, rkb/c, ws/c, wkb/c }' >> io.$d
+
 done
 u=$(( $u * $concur_mult ))
 done
 
 u=1
 while [ $u -le $max_concur ]; do
+suffix=o.bl_${use_bltl}.trx_${use_bltl}.dblwr_${use_dblw}.wthr_8.uthr_${u}.dirty_0
 grep final ${suffix}* | grep read:
 u=$(( $u * $concur_mult ))
 done | awk '{ printf "%.0f\n", $5 }' > r
 
-for d in 0 6 17 25 100 ; do paste dr.$d dw.$d r > p.$d ; done
+for d in 0 6 17 25 100 ; do paste dr.$d dw.$d r io.$d > p.$d ; done
 
 for d in 0 6 17 25 100 ; do
 
@@ -42,12 +50,13 @@ echo "w_IO - average write IOPs" >> report.$d
 echo "w_avlat - average write IO latency in millisecs" >> report.$d
 echo "w_9X - 9Xth percentile write latency in millisecs" >> report.$d
 echo "w_mxlat - max write IO latency in millisecs" >> report.$d
-echo "ro_mxIO - average read IOPs for read-only test with same concurrency" >> report.$d
+echo "ro_mx - average read IOPs for read-only test with same concurrency" >> report.$d
+echo "io.r, io.rkb, io.w, io.wkb - average rates from iostat for r/s, rkb/s, w/s, wkb/s" >> report.$d
 echo "" >> report.$d
-echo r_IO r_avlat r_95 r_99 r_99.9 r_99.99 r_mxlat w_IO w_avlat w_95 w_99 w_99.9 w_99.99 w_mxlat ro_mxIO | \
-  awk '{ printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15 }' >> report.$d
+echo r_IO r_avlat r_95 r_99 r_99.9 r_99.99 r_mxlat w_IO w_avlat w_95 w_99 w_99.9 w_99.99 w_mxlat ro_mx io_r io_rkb io_w io_wkb | \
+  awk '{ printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19 }' >> report.$d
 
-awk '{ printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15 }' p.$d >> report.$d
+awk '{ printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19 }' p.$d >> report.$d
 
 done
 
