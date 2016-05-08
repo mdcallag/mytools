f46109bf21d1a83318bcb635c4651d8a3557049a
Mark Callaghan <mdcallag@gmail.com>
2016-05-07 17:35:51 -0700

    add np250m

---
+++ bench/ibench/np250m.sh
@@ -0,0 +1,21 @@
+e=$1
+eo=$2
+client=$3
+data=$4
+dname=$5
+checku=$6
+dop=$7
+mongo=$8
+
+bash np.sh 250000000 $e "$eo" 3 $client $data  $dop 10 20 0 $dname no $checku 100 0 0 yes $mongo
+mkdir l
+mv o.* l
+
+bash np.sh   5000000 $e "$eo" 3 $client $data $dop 10 20 0 $dname no 1 100 1000 1 no $mongo
+mkdir q1000
+mv o.* q1000
+
+bash np.sh    500000 $e "$eo" 3 $client $data $dop 10 20 0 $dname no 1 100 100 1 no $mongo
+mkdir q100
+mv o.* q100
+
