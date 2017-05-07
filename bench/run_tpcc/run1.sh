e3d42bfa04ddf3e00c24c2d0513f81d990710e80
Mark Callaghan <mcallaghan@fb.com>
2017-05-07 12:42:35 -0700

    more monitoring in run1.sh

--- bench/run_tpcc/run1.sh
+++ bench/run_tpcc/run1.sh
@@ -1,131 +1,125 @@
 nw=$1
 engine=$2
 mysql=$3
-maxdop=$4
+ddir=$4
 myu=$5
 myp=$6
 myd=$7
-mysock=$8
-rt=$9
-mt=${10}
+rt=$8
+mt=$9
 
-# if yes then prepare the sbtest table else ignore
-prepare=${11}
+prepare=${10}
+run=${11}
 
-# if yes then drop the sbtest table at test end else ignore
-drop=${12}
+dbh=${12}
 
-dbh=${13}
+# extra options for create table
+create_opt=${13}
 
-# use innodb compression (yes|no)
-compress=${14}
+# number of concurrent clients
+dop=${14}
 
-# use partitioning
-part=${15}
+# suffix for output files
+sfx=${15}
 
-# path to mysql install
-mybase=${16}
+sla=${16}
 
-run_mysql="$mysql -u$myu -p$myp -h$dbh $myd -A"
+# name of storage device in iostat for database IO
+dname=${17}
+
+repint=3
+
+run_mysql="$mysql -u$myu -p$myp -h$dbh -A"
+echo $run_mysql
+
+$run_mysql -e "reset master"
+
+killall iostat
+killall vmstat
+
+python mstat.py --loops 1000000 --interval $repint --db_user=root --db_password=pw --db_host=${dbhost} >& tpc.mstat.$sfx &
+mpid=$!
+vmstat $repint >& tpc.vm.$sfx &
+vpid=$!
+iostat -kx $repint >& tpc.io.$sfx &
+ipid=$!
 
 if [[ $prepare == "yes" ]]; then
   echo prepare: drop and create db
-  $run_mysql -e "drop database test; create database test"
+  $run_mysql -e "drop database ${myd}"
+  $run_mysql -e "create database ${myd}"
+
   echo prepare: create tables
-  if [ $part != "yes" ] ; then 
-    echo Create without partitioning
-    $run_mysql < create_table.sql
-  else
-    echo Create with partitioning
-    $run_mysql < create_table_part.sql
-  fi
+
+  cat create_table.sql | sed -e "s/Engine=InnoDB/Engine=${engine} ${create_opt} DEFAULT COLLATE=latin1_bin/g" > create_table_${engine}.sql
+  cat add_fkey_idx.sql | grep -v "FOREIGN KEY" > add_fkey_idx_${engine}.sql
+
+  $run_mysql $myd < create_table_${engine}.sql
+  $run_mysql $myd < add_fkey_idx_${engine}.sql
 
   echo Running tpcc_load
-  echo ./tpcc_load $dbh $myd $myu $myp $nw
-  if ! ./tpcc_load $dbh $myd $myu $myp $nw  ; then
+  echo ./tpcc_load -h $dbh -d $myd -u $myu -p $myp -w $nw
+  if ! ./tpcc_load -h $dbh -d $myd -u $myu -p $myp -w $nw > tpc.l.o.$sfx 2> tpc.l.e.$sfx  ; then
     echo Load failed
     exit 1
   fi
-
-  echo Pre compression size
-  ssh $dbh ls -lh ${mybase}/var/${myd} | grep ibd
-
-  if [[ ${compress} == "yes" || ${compress} == "yespad" ]]; then
-    if [ ${compress} == "yespad" ]; then
-      echo "Pad for innodb compression"
-      $run_mysql -e "alter table history add column pad char(30) default ''"
-      $run_mysql -e "alter table stock add column pad char(150) default ''"
-      $run_mysql -e "alter table item add column pad char(60) default ''"
-      $run_mysql -e "alter table customer add column pad char(255) default ''"
-      $run_mysql -e "alter table orders add column pad char(20) default ''"
-      $run_mysql -e "alter table new_orders add column pad char(20) default ''"
-      $run_mysql -e "alter table order_line add column pad char(40) default ''"
-    
-      echo Post pad size
-      ssh $dbh ls -lh ${mybase}/var/${myd} | grep ibd
-    fi
-
-    echo "Use innodb compression"
-    for t in customer history new_orders orders order_line item stock  ; do
-      $run_mysql -e "alter table $t engine=innodb key_block_size=8"
-    done
-    echo Post compression size
-    ssh $dbh ls -lh ${mybase}/var/${myd} | grep ibd
-  else
-    echo "Do not use innodb compression"
-  fi
-  echo Sleep 10 seconds after running load
-  sleep 10
 fi
 
-dop=16
-while [[ $dop -le $maxdop ]]; do
+if [[ $run == "yes" ]]; then
 
-  lag=1000000
-  lag_sleep=1
-  while [[ $lag -gt 1000 ]]; do
-    sleep $lag_sleep
-    lag=$( $run_mysql -e "show engine innodb status\G" | grep History | awk '{ print $4 }' )
-    echo Wait for purge lag to drop from $lag to 1000 with dop $dop
-    lag_sleep=10
-  done
+  echo "before" > tpc.r.sz.$sfx
+  du -hs $ddir >> tpc.r.sz.$sfx
 
   echo ./tpcc_start $dbh $myd $myu $myp $nw $dop $rt $mt
-  ./tpcc_start $dbh $myd $myu $myp $nw $dop $rt $mt > tpc.o.$dop
+  echo ./tpcc_start -h $dbh -d $myd -u $myu -p $myp -w $nw -c $dop -r $rt -l $mt -0 $sla -1 $sla -2 $sla -3 $sla -4 $sla
+  ./tpcc_start -h $dbh -d $myd -u $myu -p $myp -w $nw -c $dop -r $rt -l $mt -i $repint -0 $sla -1 $sla -2 $sla -3 $sla -4 $sla > tpc.r.o.$sfx 2> tpc.r.e.$sfx
 
-  $run_mysql -e 'show mutex status' > tpc.ms.${dop}
-  dop=$(( $dop * 4 ))
+  echo "after" >> tpc.r.sz.$sfx
+  du -hs $ddir >> tpc.r.sz.$sfx
+  awk '/^MEASURING START/,/^STOPPING THREAD/' tpc.r.o.$sfx | \
+    awk '{ (if (NF == 13) { print $3 } }' | \
+    sed 's/,//' > tpc.r.qps.$sfx
+fi
 
+kill $mpid
+kill $ipid
+kill $vpid
+
+if [[ $run == "yes" ]]; then
+
+  tpmc=$( tail -1 tpc.r.o.$sfx | awk '{ print $1 }' ) > tpc.r.res.$sfx
+
+  printf "samp\tr/s\trkb/s\twkb/s\tr/tpmc\trkb/t\twkb/t\ttpmc\t\n" >> tpc.r.res.$sfx
+  grep $dname tpc.io.$sfx | awk '{ rs += $4; rkb += $6; wkb += $7; c += 1 } END { printf "%s\t%.1f\t%.0f\t%.0f\t%.3f\t%.3f\t%.3f\t%s\t\n", c, rs/c, rkb/c, wkb/c, rs/c/q, rkb/c/q, wkb/c/q, q, (p*r)/q }' q=${tpmc} p=$dop r=$rpc  >> tpc.r.res.$sfx
+
+  printf "\nsamp\tcs/s\tcpu/s\tcs/tpmc\tcpu/tpmc\n" >> tpc.r.res.$sfx
+  grep -v swpd tpc.vm.$sfx | awk '{ cs += $12; cpu += $13 + $14; c += 1 } END { printf "%s\t%.0f\t%.1f\t%.3f\t%.6f\n", c, cs/c, cpu/c, cs/c/q, cpu/c/q }' q=${tpmc} >> tpc.r.res.$sfx
+
+  cat tpc.r.sz.$sfx >> tpc.r.res.$sfx
+
+  echo >> tpc.r.res.$sfx
+  awk '/\<Raw Results/,/\<Raw Results2/' tpc.r.o.$sfx >> tpc.r.res.$sfx
+  echo >> tpc.r.res.$sfx
+  awk '/\<Constraint Check/,/\<TpmC/' tpc.r.o.$sfx >> tpc.r.res.$sfx
+fi
+
+for t in warehouse district customer history new_orders orders order_line item stock  ; do
+  $run_mysql $myd -e "show table status like \"$t\"\G" >> tpc.extra.$sfx
 done
 
 for t in warehouse district customer history new_orders orders order_line item stock  ; do
-  $run_mysql -e "show table status like \"$t\"\G"
+  $run_mysql $myd -e "show create table $t\G" >> tpc.extra.$sfx
 done
-
-for t in warehouse district customer history new_orders orders order_line item stock  ; do
-  $run_mysql -e "show create table $t\G"
-done
-
-$run_mysql -e 'select * from information_schema.innodb_cmp'
-
-for t in warehouse district customer history new_orders orders order_line item stock  ; do
-  $run_mysql -e "select COMPRESS_OPS, COMPRESS_OPS_OK, COMPRESS_USECS, COMPRESS_OK_USECS, UNCOMPRESS_OPS, UNCOMPRESS_USECS, TABLE_NAME \
-  from information_schema.table_statistics where TABLE_NAME like \"$t%\""
-done
-
-$run_mysql -e 'select (COMPRESS_OPS - COMPRESS_OPS_OK) as cfail, ((compress_ops - compress_ops_ok)/compress_ops) as failratio, TABLE_NAME from information_schema.table_statistics where COMPRESS_OPS > 0 order by cfail desc'
 
 for t in warehouse district customer history new_orders orders order_line item stock ; do
   $run_mysql -e "select ROWS_READ, ROWS_INSERTED, ROWS_UPDATED, ROWS_DELETED, TABLE_NAME \
-  from information_schema.table_statistics where TABLE_NAME like \"$t%\""
+  from information_schema.table_statistics where TABLE_NAME like \"$t%\"" >> tpc.extra.$sfx
 done
 
-$run_mysql -e 'select * from information_schema.table_statistics\G'
+$run_mysql -e "show engine ${engine} status\G" > tpc.ts.$sfx
+$run_mysql -e "show global status" > tpc.gs.$sfx
+$run_mysql -e "show global variables" > tpc.gv.$sfx
+$run_mysql -e "show memory status" > tpc.ms.$sfx
+$run_mysql -e "reset master"
 
-echo Post run size
-ssh $dbh ls -lh ${mybase}/var/${myd} | grep ibd
-
-if [[ $drop == "yes" ]]; then
-  $run_mysql -e "drop database test; create database test"
-fi
 
