591fccdadce50d6136949b6a721dab3e248d1ec2
Mark Callaghan <mdcallag@gmail.com>
2018-08-06 16:45:40 -0700

    updates

--- scripts/index_structures/heap_compare.py
+++ scripts/index_structures/heap_compare.py
@@ -79,13 +79,13 @@ def run_pop(run):
   else:
     return True
     
-def main(argv):
+def estimate_compares(sizes):
   h = [0]
   nval = 0
-  for a in argv:
-    a = int(a)
-    nval += a
-    run = [random.randint(0, 1000000000) for x in xrange(a)]
+  
+  for s in sizes:
+    nval += s
+    run = [random.randint(0, 1000000000) for x in xrange(s)]
     run.sort()
     d = deque()
     for x in run:
@@ -104,9 +104,82 @@ def main(argv):
     #print 'produce %d' % v
     assert v >= lastv
     lastv = v
-  print 'nval = %d, ncmp = %d, ocmp = %d, ncmp/v = %.2f, ocmp/v = %.2f' % (
-    nval, ncmp, ocmp, (1.0*ncmp) / nval, (1.0*ocmp) / nval)
 
+  return ncmp, ocmp, nval
+
+def fill_levels(lsm_type, maxvals, maxiters, rplto):
+  fanout_arr = [2, 4, 5, 8, 10, 15, 20]
+  
+  print("niters\t"),
+  for fanout in fanout_arr:
+    print("fo=%d\t" % fanout),
+  print("")
+
+  for niters in xrange(1, maxiters+1):
+    print("%d\t" % niters),
+    for fanout in fanout_arr:
+      curvals = maxvals
+      sizes = []
+      ok = True
+      for n in xrange(1, niters+1):
+        if curvals < 1:
+          # print 'curvals < 1 for fanout %d, vals %d, iters %d' % (fanout, maxvals, niters)
+          ok = False
+          break
+        sizes.append(curvals)
+        curvals //= fanout
+
+      if ok:
+        sizes.reverse()
+        if lsm_type == 'rpl':
+          newsizes = []
+          for x, s in enumerate(sizes):
+            newsizes.append(s)
+            if (x+1) <= rplto and (x+1) < len(sizes):
+              for x2 in xrange(fanout-1):
+                newsizes.append(s)
+          sizes = newsizes
+
+        # print 'vals %d, iters %d, fanout %d, sizes %s' % (maxvals, niters, fanout, sizes)
+        ncmp, ocmp, nval = estimate_compares(sizes)
+        ncmp_val = (1.0*ncmp) / nval
+        ocmp_val = (1.0*ocmp) / nval
+      else:
+        ncmp = ocmp = nval = ncmp_val = ocmp_val = -9
+         
+      # print '%d fo, %d niter, %d nval, %d ncmp, %d ocmp, %.2f ncmp/v, %.2f ocmp/v' % (
+      # fanout, niters, nval, ncmp, ocmp, (1.0*ncmp) / nval, (1.0*ocmp) / nval)
+      print("%.2f\t" % ncmp_val),
+    print("")
+
+def main(argv):
+  sizes = []
+  if argv[0] == 'one':
+    for a in argv[1:]:
+      sizes.append(int(a))
+
+    ncmp, ocmp, nval = estimate_compares(sizes)
+    print '%d nval, %d ncmp, %d ocmp, %.2f ncmp/v, %.2f ocmp/v' % (
+      nval, ncmp, ocmp, (1.0*ncmp) / nval, (1.0*ocmp) / nval)
+
+  elif argv[0] == 'leveled':
+    assert(len(argv) == 3)
+    maxvals = int(argv[1])
+    maxiters = int(argv[2])
+    fill_levels('leveled', maxvals, maxiters, -1)
+    
+  elif argv[0] == 'rpl':
+    assert(len(argv) == 4)
+    maxvals = int(argv[1])
+    maxiters = int(argv[2])
+    rplto = int(argv[3])
+    fill_levels('rpl', maxvals, maxiters, rplto)
+
+  else:
+    print 'arg 0 must be "one" or "leveled" or "rpl"'
+    sys.exit(-1)
+    
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
+
 
