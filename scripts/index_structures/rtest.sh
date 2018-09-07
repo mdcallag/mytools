a6f032146f15d78d03eadff431479b884b3afba0
Mark Callaghan <mdcallag@gmail.com>
2018-09-07 16:27:01 -0700

    add tests

---
+++ scripts/index_structures/rtest.sh
@@ -0,0 +1,5 @@
+python lsm_rpl_rws.py --level_config='t:1:8,t:8:8,t:8:8,t:8:8,t:8:8' --label=X2
+python lsm_rpl_rws.py --level_config='t:1:20,t:20:2,l:14:1,l:16:1' --label=X2
+python lsm_rpl_rws.py --level_config='l:8:2,l:8:2,l:8:1,l:8:1' --label=X2
+python lsm_rpl_rws.py --level_config='l:8:1,l:8:1,l:8:1,l:8:1' --label=X2
+
