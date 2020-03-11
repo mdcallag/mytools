6093434242d5a6c645eac23d2b06f46ab6e09dfa
Mark Callaghan <mdcallag@gmail.com>
2020-03-11 12:39:59 -0700

    add scripts to run for read heavy

---
+++ bench/run_linkbench/all2.sh
@@ -0,0 +1,39 @@
+fn=$1
+client=$2
+ddir=$3
+maxid=$4
+dname=$5
+wdop=$6
+secs=$7
+myORmo=$8
+ddl=$9
+dbhost=${10}
+ldop=${11}
+
+echo Load at $( date )
+bash load.sh $fn $client $ddir $maxid $dname $ldop true $myORmo $ddl $dbhost
+
+# Warmup
+echo Warmup at $( date )
+bash run.sh $fn.W.P${wdop} $client $ddir $maxid $dname $wdop $secs $myORmo $dbhost
+
+shift 11
+
+loop=1
+if [[ $# -gt 0 ]]; then
+  doparr=( "$@" )
+
+  for mydop in "${doparr[@]}" ; do
+    echo Run dop=$mydop at $( date )
+    bash run.sh $fn.L${loop}.P${mydop} $client $ddir $maxid $dname $mydop $secs $myORmo $dbhost
+    loop=$(( $loop + 1 ))
+  done
+fi
+
+mydop=1
+for wf in gll mgl gn ; do
+  bash run2.sh $fn.L${loop}.P${mydop} $client $ddir $maxid $dname $mydop $secs $myORmo $dbhost config/FBWorkload.${wf}.properties
+  loop=$(( $loop + 1 ))
+done
+
+
