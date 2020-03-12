56eb842915472d85f9fe92e755938ee8177edc44
Mark Callaghan <mdcallag@gmail.com>
2020-03-11 17:47:20 -0700

    add paths for mo40 and mo44

--- bench/run_linkbench/rall.sh
+++ bench/run_linkbench/rall.sh
@@ -5,16 +5,20 @@ secs=$4
 dbhost=$5
 ldop=$6
 dbms_cnf=$7
+up=$8
 # then DOP for queries as sequence of 1+ integers
 
 dgit=/home/mdcallag/git/mytools/bench/run_linkbench
-dpg12=/home/mdcallag/d/pg120
-dmy80=/home/mdcallag/d/my8018
-dmy57=/home/mdcallag/d/my5728
+dpg12=/home/mdcallag/d/pg12
+dmy80=/home/mdcallag/d/my80
+dmy57=/home/mdcallag/d/my57
+dmy56=/home/mdcallag/d/my56
 dmyfb=/home/mdcallag/d/fbmy56
-dmo42=/home/mdcallag/d/mo421
+dmo40=/home/mdcallag/d/mo40
+dmo42=/home/mdcallag/d/mo42
+dmo44=/home/mdcallag/d/mo44
 
-function do_rx {
+function do_rx56 {
   cnf=$1
   shift 1
 
@@ -23,7 +27,9 @@ function do_rx {
   cd $dmyfb; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
   echo start load and run at $( date )
   cd $dgit; bash all.sh rx56 ~/d/fbmy56/bin/mysql /data/m/fbmy $maxid $dname $wdop $secs mysql lb.myrocks $dbhost $ldop $@ >& a.$sfx; sleep 10
-  cd $dmyfb; bash down.sh; cd $dgit
+  if [[ $up != 1 ]]; then
+    cd $dmyfb; bash down.sh; cd $dgit
+  fi
   rdir=a.rx56.c${cnf}
   mkdir $rdir
   mv $dmyfb/o.ini.* l.* r.* a.$sfx $rdir
@@ -38,8 +44,10 @@ function do_in80 {
   sfx=in.c$cnf
   cd $dmy80; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
   echo start load and run at $( date )
-  cd $dgit; bash all.sh in80 ~/d/my8018/bin/mysql /data/m/my/data $maxid $dname $wdop $secs mysql lb.innodb $dbhost $ldop $@ >& a.$sfx; sleep 10
-  cd $dmy80; bash down.sh; cd $dgit
+  cd $dgit; bash all.sh in80 ~/d/my80/bin/mysql /data/m/my/data $maxid $dname $wdop $secs mysql lb.innodb $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmy80; bash down.sh; cd $dgit
+  fi
   rdir=a.in80.c${cnf}
   mkdir $rdir
   mv $dmy80/o.ini.* l.* r.* a.$sfx $rdir
@@ -54,15 +62,35 @@ function do_in57 {
   sfx=in.c$cnf
   cd $dmy57; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
   echo start load and run at $( date )
-  cd $dgit; bash all.sh in57 ~/d/my5728/bin/mysql /data/m/my/data $maxid $dname $wdop $secs mysql lb.innodb $dbhost $ldop $@ >& a.$sfx; sleep 10
-  cd $dmy57; bash down.sh; cd $dgit
+  cd $dgit; bash all.sh in57 ~/d/my57/bin/mysql /data/m/my/data $maxid $dname $wdop $secs mysql lb.innodb $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmy57; bash down.sh; cd $dgit
+  fi
   rdir=a.in57.c${cnf}
   mkdir $rdir
   mv $dmy57/o.ini.* l.* r.* a.$sfx $rdir
   cp $dmy57/etc/my.cnf $rdir
 }
 
-function do_pg {
+function do_in56 {
+  cnf=$1
+  shift 1
+
+  echo "innodb conf $cnf at $( date )"
+  sfx=in.c$cnf
+  cd $dmy56; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
+  echo start load and run at $( date )
+  cd $dgit; bash all.sh in56 ~/d/my56/bin/mysql /data/m/my/data $maxid $dname $wdop $secs mysql lb.innodb $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmy56; bash down.sh; cd $dgit
+  fi
+  rdir=a.in56.c${cnf}
+  mkdir $rdir
+  mv $dmy56/o.ini.* l.* r.* a.$sfx $rdir
+  cp $dmy56/etc/my.cnf $rdir
+}
+
+function do_pg12 {
   cnf=$1
   shift 1
 
@@ -70,15 +98,35 @@ function do_pg {
   sfx=pg.c$cnf
   cd $dpg12; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
   echo start load and run at $( date )
-  cd $dgit; bash all.sh pg12 ~/d/pg120/bin/psql /data/m/pg/base $maxid $dname $wdop $secs postgres lb.postgres $dbhost $ldop $@ >& a.$sfx; sleep 10
-  cd $dpg12; bash down.sh; cd $dgit
+  cd $dgit; bash all.sh pg12 ~/d/pg12/bin/psql /data/m/pg/base $maxid $dname $wdop $secs postgres lb.postgres $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dpg12; bash down.sh; cd $dgit
+  fi
   rdir=a.pg12.c${cnf}
   mkdir $rdir
   mv $dpg12/o.ini.* l.* r.* a.$sfx $rdir
   cp $dpg12/conf.diff $rdir
 }
 
-function do_mo {
+function do_mo40 {
+  cnf=$1
+  shift 1
+
+  echo "mongo conf $cnf at $( date )"
+  sfx=mo.c$cnf
+  cd $dmo40; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
+  echo start load and run at $( date )
+  cd $dgit; bash all.sh mo40 ~/d/mo40/bin/mongo /data/m/mo $maxid $dname $wdop $secs mongo lb.mongo $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmo40; bash down.sh; cd $dgit
+  fi
+  rdir=a.mo40.c${cnf}
+  mkdir $rdir
+  mv $dmo40/o.ini.* l.* r.* a.$sfx $rdir
+  cp $dmo40/mongo.conf $rdir
+}
+
+function do_mo42 {
   cnf=$1
   shift 1
 
@@ -86,12 +134,32 @@ function do_mo {
   sfx=mo.c$cnf
   cd $dmo42; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
   echo start load and run at $( date )
-  cd $dgit; bash all.sh mo42 ~/d/mo421/bin/mongo /data/m/mo $maxid $dname $wdop $secs mongo lb.mongo $dbhost $ldop $@ >& a.$sfx; sleep 10
-  cd $dmo42; bash down.sh; cd $dgit
+  cd $dgit; bash all.sh mo42 ~/d/mo42/bin/mongo /data/m/mo $maxid $dname $wdop $secs mongo lb.mongo $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmo42; bash down.sh; cd $dgit
+  fi
   rdir=a.mo42.c${cnf}
   mkdir $rdir
   mv $dmo42/o.ini.* l.* r.* a.$sfx $rdir
   cp $dmo42/mongo.conf $rdir
+}
+
+function do_mo44 {
+  cnf=$1
+  shift 1
+
+  echo "mongo conf $cnf at $( date )"
+  sfx=mo.c$cnf
+  cd $dmo44; bash ini.sh $cnf >& o.ini.$sfx; sleep 10
+  echo start load and run at $( date )
+  cd $dgit; bash all.sh mo44 ~/d/mo44/bin/mongo /data/m/mo $maxid $dname $wdop $secs mongo lb.mongo $dbhost $ldop $@ >& a.$sfx; sleep 10
+  if [[ $up != 1 ]]; then
+    cd $dmo44; bash down.sh; cd $dgit
+  fi
+  rdir=a.mo44.c${cnf}
+  mkdir $rdir
+  mv $dmo44/o.ini.* l.* r.* a.$sfx $rdir
+  cp $dmo44/mongo.conf $rdir
 }
 
 dbms=$( echo $dbms_cnf | tr '.' ' ' | awk '{ print $1 }' )
@@ -100,15 +168,21 @@ echo Run for dbms=$dbms and cnf=$cnf
 
 shift 7
 
-if [[ $dbms = "rx" ]]; then
-  do_rx $cnf $@
+if [[ $dbms = "rx56" ]]; then
+  do_rx56 $cnf $@
 elif  [[ $dbms = "in80" ]]; then
   do_in80 $cnf $@
 elif  [[ $dbms = "in57" ]]; then
   do_in57 $cnf $@
-elif [[ $dbms = "pg" ]]; then
-  do_pg $cnf $@
-elif [[ $dbms = "mo" ]]; then
-  do_mo $cnf $@
+elif  [[ $dbms = "in56" ]]; then
+  do_in56 $cnf $@
+elif [[ $dbms = "pg12" ]]; then
+  do_pg12 $cnf $@
+elif [[ $dbms = "mo40" ]]; then
+  do_mo40 $cnf $@
+elif [[ $dbms = "mo42" ]]; then
+  do_mo42 $cnf $@
+elif [[ $dbms = "mo44" ]]; then
+  do_mo44 $cnf $@
 fi
 
