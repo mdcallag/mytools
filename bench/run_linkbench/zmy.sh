d61777fbc44d705ff8b4b680c6b8042ec63d7a26
Full Name <put.your.email.address.here@mongodb.com>
2020-04-06 17:54:18 +0000

    fix replset

--- bench/run_linkbench/zmy.sh
+++ bench/run_linkbench/zmy.sh
@@ -6,6 +6,5 @@ bdir=$5
 
 for d in in80.9b40s1 pg12.7b40s1 rx56.5b40s1 rx56.6b40s1 ; do
   echo Run $d at $( date ) with $nrows rows and $nsecs secs
-  #bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 1 $d no $bdir 16 16 16 16 16 16 16 16
-  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 1 $d no $bdir 16
+  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 1 $d no $bdir 16 16 16 16 16 16 16 16
 done
