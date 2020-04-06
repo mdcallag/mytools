e948d2db4a715adc01c3b2b433c407888cf177d5
Full Name <put.your.email.address.here@mongodb.com>
2020-04-06 19:01:28 +0000

    helper

--- bench/run_linkbench/zmo-dsi.sh
+++ bench/run_linkbench/zmo-dsi.sh
@@ -10,8 +10,7 @@ for ver in mo4016 mo4017 ; do
   echo Run $cnf and $ver at $( date ) for $nrows rows and $nsecs secs
   rm $bdir/mo40
   ln -s $bdir/$ver $bdir/mo40
-  #bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo40.${cnf} no $bdir 16 16 16 16 16 16
-  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo40.${cnf} no $bdir 16 
+  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo40.${cnf} no $bdir 16 16 16 16 16 16
   mv a.mo40.c${cnf} a.${ver}.c${cnf}
 done
 
@@ -20,8 +19,7 @@ for ver in mo421 mo423 mo425 ; do
   echo Run $cnf and $ver at $( date ) for $nrows rows and $nsecs secs
   rm $bdir/mo42
   ln -s $bdir/$ver $bdir/mo42
-  #bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo42.${cnf} no $bdir 16 16 16 16 16 16
-  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo42.${cnf} no $bdir 16 
+  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo42.${cnf} no $bdir 16 16 16 16 16 16
   mv a.mo42.c${cnf} a.${ver}.c${cnf}
 done
 
@@ -30,8 +28,7 @@ for ver in mo44pre ; do
   echo Run $cnf and $ver at $( date ) for $nrows rows and $nsecs secs
   rm $bdir/mo44
   ln -s $bdir/$ver $bdir/mo44
-  #bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo44.${cnf} no $bdir 16 16 16 16 16 16
-  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo44.${cnf} no $bdir 16 
+  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo44.${cnf} no $bdir 16 16 16 16 16 16
   mv a.mo44.c${cnf} a.${ver}.c${cnf}
 done
 
