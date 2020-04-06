5223606f0722ac138678302623201a651f20aa4b
Full Name <put.your.email.address.here@mongodb.com>
2020-04-06 19:09:19 +0000

    arg for ldop

--- bench/run_linkbench/zmy.sh
+++ bench/run_linkbench/zmy.sh
@@ -1,10 +1,11 @@
 nrows=$1
 nsecs=$2
-dev=$3
-wdop=$4
-bdir=$5
+bdir=$3
+dev=$4
+wdop=$5
+ldop=$6
 
 for d in in80.9b40s1 pg12.7b40s1 rx56.5b40s1 rx56.6b40s1 ; do
   echo Run $d at $( date ) with $nrows rows and $nsecs secs
-  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 1 $d no $bdir 16 16 16 16 16 16 16 16
+  bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop $d no $bdir 16 16 16 16 16 16
 done
