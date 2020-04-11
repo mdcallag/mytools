68cfa12e432a8c7cba1ff5898584ff6c032d2b69
Full Name <put.your.email.address.here@mongodb.com>
2020-04-11 15:36:55 +0000

    remove and recreate pg dir

--- bench/conf/nuc8i7.ub1804/pg12/ini.sh
+++ bench/conf/nuc8i7.ub1804/pg12/ini.sh
@@ -2,8 +2,7 @@ bdir=/data/m/pg
 
 echo "stop and remove files"
 bin/pg_ctl -D $bdir stop
-rm -rf $bdir/*
-mkdir -p $bdir
+rm -rf $bdir; mkdir -p $bdir
 rm -f logfile; touch logfile
 killall mysqld mongod
 
