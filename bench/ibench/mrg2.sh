eef7da5c454c7b6a343fb525b48c77a4c08ef182
Full Name <put.your.email.address.here@mongodb.com>
2020-04-14 20:16:32 +0000

    fix for vsz and rss

--- bench/ibench/mrg2.sh
+++ bench/ibench/mrg2.sh
@@ -1,13 +1,13 @@
 first_file=$( ls o.sum.* | head -1 )
 
-#ips     qps     rps     rkbps   wkbps   rpq     rkbpq   wkbpq   csps    cpups   cspq    cpupq   dbgb    maxop   p50     p99     tag
-#99.8    7071.3  0.0     0       93      0.000   0.000   0.013   27192   26.1    3.845   0.003690        2.3     0.019   7074.6  7032.9  5m.in57.c8
+#ips     qps     rps     rkbps   wkbps   rpq     rkbpq   wkbpq   csps    cpups   cspq    cpupq   	dbgb    vsz     rss     maxop   p50     p99     tag
+#5041.3	0       0.3     3       197213  0.000   0.000   3.910   18387   71.1    0.365   0.001409        64      25.1    23.3    0.477   8348.6  3050.4  80m.mo425.c5b
 
 for l in 2 3 4; do
   head -1 $first_file > mrg.q$l
   for f in o.sum.*; do
     head -${l} $f | tail -1 | \
-      awk -v FS='\t' -v OFS='\t' '{ $1=sprintf("%.0f", $1); $2=sprintf("%.0f", $2); $12=sprintf("%.0f", $12 * 1000000); $15=sprintf("%.0f", $15); $16=sprintf("%.0f", $16); print $0 }'
+      awk -v FS='\t' -v OFS='\t' '{ $1=sprintf("%.0f", $1); $2=sprintf("%.0f", $2); $12=sprintf("%.0f", $12 * 1000000); $17=sprintf("%.0f", $17); $18=sprintf("%.0f", $18); print $0 }'
   done  >> mrg.q$l
 done
 
