3c5d7a1f9d5acffda29befe6c79d9dd0b096114a
Full Name <put.your.email.address.here@mongodb.com>
2020-04-15 14:17:53 +0000

    touch pages

--- bench/mlock.c
+++ bench/mlock.c
@@ -3,10 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void touch_pages(char *start, int n_pages) {
+  int x;
+  for (x=0; x < n_pages; x++) {
+    *start = x;
+    start += 4096;
+  } 
+}
+
 int main(int argc, char **argv) {
   int mb_to_alloc;
   int mb_per_alloc;
-  int m;
+  int m,x;
+  int n_allocs;
+  int n_pages;
+  char **allocs;
 
   if (argc < 3) {
     fprintf(stderr, "need 2 args: gb to lock, mb per alloc\n");
@@ -19,13 +30,23 @@ int main(int argc, char **argv) {
   fprintf(stdout, "Lock %d GB, malloc %d mb chunks\n", mb_to_alloc, mb_per_alloc);
   mb_to_alloc *= 1024;
 
-  for (m=0; m < mb_to_alloc; m += mb_per_alloc) {
+  n_pages = (mb_per_alloc * 1024) / 4;           /* number of 4k pages per allocation */
+  n_allocs = (mb_to_alloc / mb_per_alloc) + 1;
+  allocs = malloc(sizeof(char*) * n_allocs);
+
+  for (x=0, m=0; m < mb_to_alloc; x += 1, m += mb_per_alloc) {
     char *c = malloc(mb_per_alloc * 1024 * 1024);
     mlock(c, mb_per_alloc * 1024 * 1024);
+    allocs[x] = c;
+    touch_pages(c, n_pages);
   }
 
+  x = 0;
   while (1) {
-    fprintf(stdout, "sleep after locking\n");
-    sleep(60);
+    touch_pages(allocs[x], n_pages);
+    x++;
+    if (x >= n_allocs)
+      x = 0;
+    sleep(1);
   }
 }
