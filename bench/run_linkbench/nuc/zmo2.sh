437056f478ec5641cf0efdc885a648efe768356e
Mark Callaghan <mdcallag@gmail.com>
2020-04-20 08:33:03 -0700

    add sleep print

--- bench/run_linkbench/nuc/zmo2.sh
+++ bench/run_linkbench/nuc/zmo2.sh
@@ -11,8 +11,9 @@ for ver in mo44pre mo440rc0 mo440rc1 ; do
   rm $bdir/mo44
   ln -s $bdir/$ver $bdir/mo44
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo44.${cnf} no $bdir 1 1 1 1 1 1
-  sleep 1200
   mv a.mo44.c${cnf} a.${ver}.c${cnf}
+  echo Sleep 20 minutes to let HW rest
+  sleep 1200
 done
 done
 
@@ -22,8 +23,9 @@ for ver in mo425 mo424 mo423 mo422 mo421 ; do
   rm $bdir/mo42
   ln -s $bdir/$ver $bdir/mo42
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo42.${cnf} no $bdir 1 1 1 1 1 1
-  sleep 1200
   mv a.mo42.c${cnf} a.${ver}.c${cnf}
+  echo Sleep 20 minutes to let HW rest
+  sleep 1200
 done
 done
 
@@ -33,8 +35,9 @@ for ver in mo4018 mo4017 mo4016 ; do
   rm $bdir/mo40
   ln -s $bdir/$ver $bdir/mo40
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop mo40.${cnf} no $bdir 1 1 1 1 1 1
-  sleep 1200
   mv a.mo40.c${cnf} a.${ver}.c${cnf}
+  echo Sleep 20 minutes to let HW rest
+  sleep 1200
 done
 done
 
