fb38744a69a2391c54fd172f4bafb527d5a23aaf
Mark Callaghan <mdcallag@gmail.com>
2020-04-19 21:00:23 -0700

    reduce nuc dop to 1

--- bench/ibench/nuc/my/ra.sh
+++ bench/ibench/nuc/my/ra.sh
@@ -1,6 +1,6 @@
 mbd=$1
 dev=$2
 
-bash ra1.sh   5000000   5m 8 3600 $mbd $dev
-bash ra2.sh  50000000  50m 8 3600 $mbd $dev
-bash ra2.sh 250000000 250m 8 3600 $mbd $dev
+bash ra1.sh   5000000   5m 1 3600 $mbd $dev
+bash ra2.sh  50000000  50m 1 3600 $mbd $dev
+bash ra2.sh 250000000 250m 1 3600 $mbd $dev
