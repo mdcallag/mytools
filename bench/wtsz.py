b599a2948cb35c77f84fbe3e7789d1ac74bb37b4
Full Name <put.your.email.address.here@mongodb.com>
2020-05-17 21:50:49 +0000

    updates

--- bench/wtsz.py
+++ bench/wtsz.py
@@ -1,6 +1,6 @@
 
-# for t in link node count; do cp r.stats.$t.*.L6.* o.$t.x; vi o.$t.x; cat o.$t.x | sed -r 's/NumberLong\("([0-9][0-9]*)"\)/\1/' | sed -r 's/NumberLong\(([0-9][0-9]*)\)/\1/' | python3 ~/git/mytools/bench/wtsz.py > o.$t.x2; cat o.$t.x2 ; done
-# cp o.es1.dop8.ns3 o.tcm1; vi o.tcm1; cat o.tcm1 | sed -r 's/NumberLong\("([0-9][0-9]*)"\)/\1/' | sed -r 's/NumberLong\(([0-9][0-9]*)\)/\1/' > o.tcm2; cat o.tcm2 | python3 ../../tcm.py > o.tcm3; cat o.tcm3
+# cat o.tab1.id.dop8.ns3 | sed -r 's/NumberLong\("([0-9][0-9]*)"\)/\1/' | sed -r 's/NumberLong\(([0-9][0-9]*)\)/\1/' > o.tab1.x; vi o.tab1.x; cat o.tab1.x | python3 ~/git/mytools/bench/wtsz.py > o.tab1.x2; cat o.tab1.x2
+# for t in link node count; do cp r.stats.$t.*.L6.* o.$t.x; vi o.$t.x; cat o.$t.x | sed -r 's/NumberLong\("([0-9][0-9]*)"\)/\1/' | sed -r 's/NumberLong\(([0-9][0-9]*)\)/\1/' | python3 ~/git/mytools/bench/wtsz.py > o.$t.x2; done ; cat o.link.x2 o.count.x2 o.node.x2
 
 # wiredTiger
 #   block-manager
@@ -16,8 +16,8 @@ import json
 import sys
 
 remap = {
-  "file bytes available for reuse" : "Mreuse",
   "file size in bytes" : "Msize",
+  "file bytes available for reuse" : "Mreuse",
   "bytes currently in the cache" : "Mcache",
   "bytes read into cache" : "GBread",
   "bytes written from cache" : "GBwrite",
@@ -25,8 +25,8 @@ remap = {
   "pages written from cache" : "Mpwrite" }
 
 def print_one(n, o):
-  print('%.0f\t' % (o['block-manager']['file bytes available for reuse'] / (1024 * 1024)), end='')
   print('%.0f\t' % (o['block-manager']['file size in bytes'] / (1024 * 1024)), end='')
+  print('%.0f\t' % (o['block-manager']['file bytes available for reuse'] / (1024 * 1024)), end='')
   print('%.0f\t' % (o['cache']['bytes currently in the cache'] / (1024 * 1024)), end='')
   print('%.1f\t' % (o['cache']['bytes read into cache'] / (1024 * 1024 * 1024)), end='')
   print('%.1f\t' % (o['cache']['bytes written from cache'] / (1024 * 1024 * 1024)), end='')
@@ -42,7 +42,7 @@ def main(argv):
     print('%s\t:\t%s' % (v,k))
   print()
 
-  print('Mreuse\tMsize\tMcache\tMbread\tMbwrite\tMpread\tMpwrite\tname')
+  print('Msize\tMreuse\tMcache\tMbread\tMbwrite\tMpread\tMpwrite\tname')
 
   print_one(j['ns'], j['wiredTiger'])
 
