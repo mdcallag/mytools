e628e5d525fa4ea0f804d9e3d05fa29c04463e8c
Full Name <put.your.email.address.here@mongodb.com>
2020-06-26 23:37:54 +0000

    change default tests to run

--- bench/ibench/dsi/mo/ramo.sh
+++ bench/ibench/dsi/mo/ramo.sh
@@ -4,7 +4,7 @@ dev=$2
 ips="100 100 200 200 400 400 600 600 800 800 1000 1000"
 
 ns=300
-bash ramo1.sh  10000000  10m 1 $ns $mbd $dev $ips
+bash ramo1.sh  20000000  20m 1 $ns $mbd $dev $ips
 
 ns=1800
 bash ramo1.sh  80000000  80m 8 $ns $mbd $dev $ips
--- bench/ibench/dsi/my/ra.sh
+++ bench/ibench/dsi/my/ra.sh
@@ -4,9 +4,9 @@ dev=$2
 ips="100 100 200 200 400 400 600 600 800 800 1000 1000"
 
 ns=300
-bash ra1.sh   10000000   10m 10 $ns $mbd $dev $ips
+bash ra1.sh   20000000   20m 1 $ns $mbd $dev $ips
 
 ns=1800
-bash ra1.sh   80000000   80m 10 $ns $mbd $dev $ips
-bash ra2.sh  800000000  800m 10 $ns $mbd $dev $ips
+bash ra1.sh   80000000   80m 8 $ns $mbd $dev $ips
+bash ra2.sh  800000000  800m 8 $ns $mbd $dev $ips
 
