a5bdb8581c25797db96c1feaea3030362bd52649
Mark Callaghan <mdcallag@gmail.com>
2020-06-30 18:50:55 +0000

    change concurrency levels

--- bench/run_linkbench/dsi/zmo.sh
+++ bench/run_linkbench/dsi/zmo.sh
@@ -2,11 +2,11 @@ dev=$1
 
 heap=2000
 
-bash zmo1.sh  4000001 3600 /media/ephemeral1 $dev 1 1 $heap 1
-mkdir 10m; mv a.* 4m
+bash zmo1.sh  4000001 1800 /media/ephemeral1 $dev 1 1 $heap 1 2 4 8
+mkdir 4m; mv a.* 4m
 
-bash zmo1.sh  10000001 3600 /media/ephemeral1 $dev 8 8 $heap 2 4 8 8 8 8
+bash zmo1.sh  10000001 3600 /media/ephemeral1 $dev 8 8 $heap 8 8 12 12 16 16
 mkdir 10m; mv a.* 10m
 
-bash zmo2.sh 200000001 3600 /media/ephemeral1 $dev 12 12 $heap 2 4 8 12 12 12
+bash zmo2.sh 200000001 3600 /media/ephemeral1 $dev 8 8 $heap 8 8 12 12 16 16
 mkdir 200m; mv a.* 200m
