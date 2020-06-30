f3d53febb2121122fb9863edf344db91c9c2708d
Mark Callaghan <mdcallag@gmail.com>
2020-06-30 18:56:33 +0000

    use new configs

--- bench/run_linkbench/dsi/zmy2.sh
+++ bench/run_linkbench/dsi/zmy2.sh
@@ -8,7 +8,7 @@ ldop=$6
 shift 7
 
 for ver in my8018 my8020 ; do
-for cnf in 9b40s1 ; do
+for cnf in 10b40s1 ; do
   echo Run $cnf and $ver at $( date ) with $nrows rows and $nsecs secs
   rm $bdir/my80; ln -s $bdir/$ver $bdir/my80
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop in80.${cnf} no $bdir $heap "$@"
@@ -17,7 +17,7 @@ done
 done
 
 for ver in pg123 pg120 ; do
-for cnf in 7b40s1 ; do
+for cnf in 8b40s1 ; do
   echo Run $cnf and $ver at $( date ) with $nrows rows and $nsecs secs
   rm $bdir/pg12; ln -s $bdir/$ver $bdir/pg12
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop pg12.${cnf} no $bdir $heap "$@"
@@ -25,7 +25,7 @@ for cnf in 7b40s1 ; do
 done
 done
 
-for d in rx56.6b40s1 rx56.5b40s1 ; do
+for d in rx56.8b40s1 rx56.7b40s1 ; do
   echo Run $d at $( date ) with $nrows rows and $nsecs secs
   bash rall.sh $nrows $dev $wdop $nsecs 127.0.0.1 $ldop $d no $bdir $heap "$@"
 done
