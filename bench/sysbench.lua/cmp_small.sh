c63428cc2764b86384624c73b1af38216c54d2e3
Mark Callaghan <mdcallag@gmail.com>
2020-12-17 14:26:35 -0800

    helper script to run all_small for many DBMS + configurations

---
+++ bench/sysbench.lua/cmp_small.sh
@@ -0,0 +1,50 @@
+ntabs=$1
+nrows=$2
+nsecs=$3
+basedir=$4
+sysbdir=$5
+datadir=$6
+devname=$7
+
+# TODO, support=0
+usepk=1
+
+# Remaining args are numbers of threads for which to run benchmarks, "1 2 4"
+shift 7
+
+#client=/home/mdcallag/d/my8022/bin/mysql
+#sysbdir=/home/mdcallag/d/sysbench
+#ddir=/data/m/my/data
+#dname=nvem0n1
+
+function dbms_up() {
+  bdir=$1
+  cnf=$2
+  # /media/ephemeral1 or /home/mdcallag/d or ???
+  cdir=$PWD
+  cd $bdir; bash ini.sh $cnf >& o.ini; sleep 10; cd $cdir
+}
+
+function dbms_down() {
+  bdir=$1
+  cnf=$2
+  cdir=$PWD
+  cd $bdir; bash down.sh $cnf >& o.down; cd $cdir
+}
+
+dbcreds=mysql,root,pw,127.0.0.1,test,innodb
+for dcnf in my8022.x1 my8021.x1 my8020.x1 my8019.x1 my8018.x1 ; do
+  dbms=$( echo $dcnf | tr '.' ' ' | awk '{ print $1 }' )
+  cnf=$( echo $dcnf | tr '.' ' ' | awk '{ print $2 }' )
+
+  dbdir=$basedir/$dbms
+  client=$dbdir/bin/mysql
+  echo Run for $dbms with $cnf config from $dbdir
+
+  dbms_up $dbdir $cnf
+  bash all_small.sh $ntabs $nrows $nsecs $nsecs $nsecs $dbcreds 1 1 $client none $sysbdir $datadir $devname $usepk $@
+  mkdir x.$dcnf; mv sb.* x.$dcnf; cp $dbdir/etc/my.cnf $dbdir/o.ini x.$dcnf
+  dbms_down $dbdir $cnf 
+done
+
+
