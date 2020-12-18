d6717068018716b18ceaac8d41241dc7ce596b6d
Mark Callaghan <mdcallag@gmail.com>
2020-12-18 14:27:59 -0800

    add configs

--- bench/sysbench.lua/cmp_small.sh
+++ bench/sysbench.lua/cmp_small.sh
@@ -5,17 +5,10 @@ basedir=$4
 sysbdir=$5
 datadir=$6
 devname=$7
-
-# TODO, support=0
-usepk=1
+usepk=$8
 
 # Remaining args are numbers of threads for which to run benchmarks, "1 2 4"
-shift 7
-
-#client=/home/mdcallag/d/my8022/bin/mysql
-#sysbdir=/home/mdcallag/d/sysbench
-#ddir=/data/m/my/data
-#dname=nvem0n1
+shift 8
 
 function dbms_up() {
   bdir=$1
@@ -32,23 +25,8 @@ function dbms_down() {
   cd $bdir; bash down.sh $cnf >& o.down; cd $cdir
 }
 
-dbcreds=postgres,root,pw,127.0.0.1,ib
-for dcnf in pg131 pg130 pg124 pg123 pg122 pg121 pg120 ; do
-  dbms=$( echo $dcnf | tr '.' ' ' | awk '{ print $1 }' )
-  cnf=$( echo $dcnf | tr '.' ' ' | awk '{ print $2 }' )
-
-  dbdir=$basedir/$dbms
-  client=$dbdir/bin/psql
-  echo Run for $dbms with $cnf config from $dbdir
-
-  dbms_up $dbdir $cnf
-  bash all_small.sh $ntabs $nrows $nsecs $nsecs $nsecs $dbcreds 1 1 $client none $sysbdir $datadir $devname $usepk $@
-  mkdir x.$dcnf; mv sb.* x.$dcnf; cp $dbdir/etc/cnf.diff $dbdir/o.ini x.$dcnf
-  dbms_down $dbdir $cnf 
-done
-
 dbcreds=mysql,root,pw,127.0.0.1,test,innodb
-for dcnf in my8022.x1 my8021.x1 my8020.x1 my8019.x1 my8018.x1 my8017.x1 ; do
+for dcnf in my5649.x6d my5731.x6d my8022.x6d my8021.x6d my8020.x6d my8019.x6d my8018.x6d my8017.x6d ; do
   dbms=$( echo $dcnf | tr '.' ' ' | awk '{ print $1 }' )
   cnf=$( echo $dcnf | tr '.' ' ' | awk '{ print $2 }' )
 
@@ -58,6 +36,22 @@ for dcnf in my8022.x1 my8021.x1 my8020.x1 my8019.x1 my8018.x1 my8017.x1 ; do
 
   dbms_up $dbdir $cnf
   bash all_small.sh $ntabs $nrows $nsecs $nsecs $nsecs $dbcreds 1 1 $client none $sysbdir $datadir $devname $usepk $@
-  mkdir x.$dcnf; mv sb.* x.$dcnf; cp $dbdir/etc/my.cnf $dbdir/o.ini x.$dcnf
+  mkdir x.$dcnf.pk${usepk}; mv sb.* x.$dcnf.pk${usepk}; cp $dbdir/etc/my.cnf $dbdir/o.ini x.$dcnf.pk${usepk}
   dbms_down $dbdir $cnf 
 done
+
+dbcreds=postgres,root,pw,127.0.0.1,ib
+for dcnf in pg131.x5 pg130.x5 pg124.x5 pg123.x5 pg122.x5 pg121.x5 pg120.x5 ; do
+  dbms=$( echo $dcnf | tr '.' ' ' | awk '{ print $1 }' )
+  cnf=$( echo $dcnf | tr '.' ' ' | awk '{ print $2 }' )
+
+  dbdir=$basedir/$dbms
+  client=$dbdir/bin/psql
+  echo Run for $dbms with $cnf config from $dbdir
+
+  dbms_up $dbdir $cnf
+  bash all_small.sh $ntabs $nrows $nsecs $nsecs $nsecs $dbcreds 1 1 $client none $sysbdir $datadir $devname $usepk $@
+  mkdir x.$dcnf.pk${usepk}; mv sb.* x.$dcnf.pk${usepk}; cp $dbdir/etc/cnf.diff $dbdir/o.ini x.$dcnf.pk${usepk}
+  dbms_down $dbdir $cnf 
+done
+
