3325fafe1e1f3e4c7a1989043c9d20d2aa6fc30c
Mark Callaghan <mcallaghan@fb.com>
2022-04-07 08:27:28 -0700

    updates

--- bench/rx/x3.sh
+++ bench/rx/x3.sh
@@ -1,9 +1,9 @@
 nt=$1
 v4v5=$2
 secs=$3
+dio=$4
 
 # secs=1800
-dio=0
 
 if [ $nt -eq 1 ]; then
   cx=c4
@@ -25,37 +25,49 @@ else
   exit 1
 fi
 
-for dn in res.byrx res.byos res.iobuf ; do
+for dn in res.byrx res.byos res.iobuf res.iodir ; do
   if [ -d $dn ]; then echo "$dn exists"; exit 1; fi
 done
-for dn in res.byrx res.byos res.iobuf ; do mkdir $dn; done
+for dn in res.byrx res.byos res.iobuf res.iodir ; do mkdir $dn; done
 
 nk=$nk1
 comp=none
 cfg=${cx}${rx}
 if [ $v4v5 == "yes" ]; then
-  bash x.v4.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-  bash x.v5.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
+  bash x.v4.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+  bash x.v5.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
 fi
-bash x.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-mv bm.* res.v4v5v6.byrx
+bash x.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+mv bm.* res.byrx
 
 nk=$nk1
 comp=none
 cfg=${cx}bc1g
 if [ $v4v5 == "yes" ]; then
-  bash x.v4.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-  bash x.v5.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
+  bash x.v4.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+  bash x.v5.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
 fi
-bash x.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-mv bm.* res.v4v5v6.byos
+bash x.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+mv bm.* res.byos
 
 nk=$nk2
 comp=lz4
 cfg=${cx}${rx}
 if [ $v4v5 == "yes" ]; then
-  bash x.v4.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-  bash x.v5.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
+  bash x.v4.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+  bash x.v5.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
 fi
-bash x.sh $cfg $secs $secs $nk $nt $dio $comp 1 400 0.25 1 0
-mv bm.* res.v4v5v6.iobuf
+bash x.sh $cfg $secs $secs $nk $nt 0 $comp 1 400 0.25 1 0
+mv bm.* res.iobuf
+
+if [ $dio == "yes" ]; then
+nk=$nk2
+comp=lz4
+cfg=${cx}${rx}
+if [ $v4v5 == "yes" ]; then
+  bash x.v4.sh $cfg $secs $secs $nk $nt 1 $comp 1 400 0.25 1 0
+  bash x.v5.sh $cfg $secs $secs $nk $nt 1 $comp 1 400 0.25 1 0
+fi
+bash x.sh $cfg $secs $secs $nk $nt 1 $comp 1 400 0.25 1 0
+mv bm.* res.iodir
+fi
