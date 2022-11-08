d6dc83d9c60ca706643bf390c08b917771ceeaf3
Mark Callaghan <mdcallag@gmail.com>
2022-11-08 12:04:14 -0800

    updates

--- bench/blob.bmark/rfio.sh
+++ bench/blob.bmark/rfio.sh
@@ -6,7 +6,7 @@ filegb=$3
 for iot in dir buf ; do
   bash do_fio.sh 1 600 4096 /dev/nvme0n1 $iot /data/m $nfiles $filegb yes $ioengine | tee out.fio.$iot.4096.$ioengine
   bash do_fio.sh 1 600 8192 /dev/nvme0n1 $iot /data/m $nfiles $filegb  no $ioengine | tee out.fio.$iot.8192.$ioengine
-  mkdir res.me.$iot
-  mv out.fio.* o.fio.* res.me.$iot
+  mkdir res.$iot.$ioengine
+  mv out.fio.* o.fio.* res.$iot.$ioengine
 done
 
