4c70b8c3334879e5528d0863300fdd3ffad16c45
Mark Callaghan <mdcallag@gmail.com>
2022-11-08 17:20:58 -0800

    pass devname as arg

--- bench/blob.bmark/rfio.sh
+++ bench/blob.bmark/rfio.sh
@@ -2,10 +2,11 @@
 ioengine=$1
 nfiles=$2
 filegb=$3
+devname=$4
 
 for iot in dir buf ; do
-  bash do_fio.sh 1 600 4096 /dev/nvme0n1 $iot /data/m $nfiles $filegb yes $ioengine | tee out.fio.$iot.4096.$ioengine
-  bash do_fio.sh 1 600 8192 /dev/nvme0n1 $iot /data/m $nfiles $filegb  no $ioengine | tee out.fio.$iot.8192.$ioengine
+  bash do_fio.sh 1 600 4096 $devname $iot /data/m $nfiles $filegb yes $ioengine | tee out.fio.$iot.4096.$ioengine
+  bash do_fio.sh 1 600 8192 $devname $iot /data/m $nfiles $filegb  no $ioengine | tee out.fio.$iot.8192.$ioengine
   mkdir res.$iot.$ioengine
   mv out.fio.* o.fio.* res.$iot.$ioengine
 done
