ca25c6a8a958295776bfb35b614be270c61a9811
Mark Callaghan <mdcallag@gmail.com>
2023-05-23 18:25:59 +0000

    adjust rows for l.i0

--- bench/ibench/ra.sh
+++ bench/ibench/ra.sh
@@ -8,7 +8,12 @@ secs=$7
 delete_per_insert=$8
 
 ips="100 500 1000"
-bash ra1.sh  20000000 20000000  20m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
+if [[ $delete_per_insert == "no" ]]; then
+  nr2=20000000
+else
+  nr2=50000000
+fi
+bash ra1.sh  20000000 $nr2 20m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
 
 #ips="100 500 1000"
 #bash ra1.sh 500000000 10000000 500m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
