6908b9a6f5ace4a34b62082fca87e94e737736b8
Mark Callaghan <mdcallag@gmail.com>
2023-05-27 16:23:08 +0000

    new args

--- bench/ibench/ra.sh
+++ bench/ibench/ra.sh
@@ -6,14 +6,15 @@ npart=$5
 dop=$6
 secs=$7
 delete_per_insert=$8
+mr1=$9
+mr2=${10}
+
+nr1=$(( $mr1 * 1000000 * $dop ))
+nrm=$(( $mr1 * $dop ))
+nr2=$(( $mr2 * 1000000 * $dop ))
 
 ips="100 500 1000"
-if [[ $delete_per_insert == "no" ]]; then
-  nr2=20000000
-else
-  nr2=50000000
-fi
-bash ra1.sh  20000000 $nr2 20m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
+bash ra1.sh  $nr1 $nr2 ${nrm}m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
 
 #ips="100 500 1000"
 #bash ra1.sh 500000000 10000000 500m $dop $secs $mbd $rdir $dev $only1t $npart $delete_per_insert $ips
