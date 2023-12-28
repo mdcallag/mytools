6cfdcf9c77f5a90fd4a8a94f0e12f9334f67eee4
Mark Callaghan <mdcallag@gmail.com>
2023-12-28 02:50:41 +0000

    add vac_my to match vac_pg

--- bench/ibench/iq.sh
+++ bench/ibench/iq.sh
@@ -39,36 +39,134 @@ if [[ $delete_per_insert == "yes" || $delete_per_insert == "1" ]]; then
   fi
 fi
 
-function vac_all {
-  blocking=$1
+function vac_pg {
 
-  pga="-h 127.0.0.1 -U root ib"
+  # Sleep for 60 + 1 second per 1M rows
+  sleep_secs=$( echo $nr $ntabs | awk '{ printf "%.0f", ((($1 * $2) / 1000000) + 1) + 60 }' )
+  echo "pg_vac starts at $( date ) with sleep_secs = $sleep_secs" > o.pgvac
+  start_secs=$( date +%s )
+  done_secs=$(( start_secs + sleep_secs ))
+
+  pga=( -h 127.0.0.1 -U root ib )
   x=0
   for n in $( seq 1 $ntabs ) ; do
     if [[ $npart -eq 0 ]]; then
-      PGPASSWORD="pw" $client $pga -x -c "vacuum (verbose) pi${n}" >& o.pgvac.pi${n} &
+      PGPASSWORD="pw" $client "${pga[@]}" -x -c "vacuum (verbose, analyze) pi${n}" >& o.pgvac.pi${n} &
       vpid[${x}]=$!
       x=$(( $x + 1 ))
     else
       for p in $( seq 0 $(( $npart - 1 )) ); do
-        PGPASSWORD="pw" $client $pga -x -c "vacuum (verbose) pi${n}_p${p}" >& o.pgvac.pi${n}.p${p} &
+        PGPASSWORD="pw" $client "${pga[@]}" -x -c "vacuum (verbose, analyze) pi${n}_p${p}" >& o.pgvac.pi${n}.p${p} &
         vpid[${x}]=$!
         x=$(( $x + 1 ))
       done
     fi
   done
 
-  if [[ $blocking -eq 1 ]]; then
-    for n in $( seq 0 $(( $x - 1 )) ) ; do
-      echo After load: wait for vacuum $n
-      wait ${vpid[${n}]}
-    done
+  for n in $( seq 0 $(( $x - 1 )) ) ; do
+    echo After load: wait for vacuum $n >> o.pgvac
+    wait ${vpid[${n}]}
+  done
+
+  echo "Checkpoint started at $( date )" >> o.pgvac
+  PGPASSWORD="pw" $client "${pga[@]}" -x -c "checkpoint" 2>&1 >> o.pgvac
+  echo "Checkpoint done at $( date )" >> o.pgvac
+
+  now_secs=$( date +%s )
+  if [[ now_secs -lt done_secs ]]; then
+    diff_secs=$(( done_secs - now_secs ))
+    echo Sleep $diff_secs >> o.pgvac
+    sleep $diff_secs
   fi
+
+  now_secs=$( date +%s )
+  diff_secs=$(( now_secs - start_secs ))
+  echo "vac_pg done after $diff_secs at $( date )" >> o.pgvac
 }
 
-# if =1 then only vacuum once, after index create
-# if > 1 then vacuum after index create and before each read-write step (except the first)
-# vacuum after index create is blocking, others are not
+function vac_my {
+  # This is optional and can be run after a test step
+  # Goals:
+  #  1) Flush dirty data (b-tree writeback, LSM compaction), but without shutting down the DBMS
+  #  2) Get LSM tree into deterministic state
+  #  3) Collect stats
+
+  # Sleep for 60 + 1 second per 1M rows
+  sleep_secs=$( echo $nr $ntabs | awk '{ printf "%.0f", ((($1 * $2) / 1000000) + 1) + 60 }' )
+  echo "vac_my starts at $( date ) with sleep_secs = $sleep_secs" > o.myvac
+  start_secs=$( date +%s )
+  done_secs=$(( start_secs + sleep_secs ))
+
+  mya=( -h127.0.0.1 -uroot -ppw ib )
+
+  # Reduce chance of a full disk
+  $client "${mya[@]}" -e 'reset master' 2> /dev/null
+
+  for x in $( seq 1 $ntabs ); do
+    echo Analyze table pi${x} >> o.myvac
+    /usr/bin/time -o o.myvac.time.$x $client "${mya[@]}" -e "analyze table pi${x}" > o.myvac.at.$x 2>&1 &
+    apid[${x}]=$!
+  done
+
+  if [[ $e == "innodb" ]]; then
+    maxDirty=$( $client "${mya[@]}" -N -B -e 'show global variables like "innodb_max_dirty_pages_pct"' | awk '{ print $2 }' )
+    maxDirtyLwm=$( $client "${mya[@]}" -N -B -e 'show global variables like "innodb_max_dirty_pages_pct_lwm"' | awk '{ print $2 }' )
+    # This option is only in 8.0.18+
+    idlePct=$( $client "${mya[@]}" -N -B -e 'show global variables like "innodb_idle_flush_pct"' 2> /dev/null | awk '{ print $2 }' )
+
+    echo "Reduce max_dirty to 0 to flush InnoDB buffer pool" >> o.myvac
+    $client "${mya[@]}" -e 'set global innodb_max_dirty_pages_pct_lwm=1' >> o.myvac 2>&1
+    $client "${mya[@]}" -e 'set global innodb_max_dirty_pages_pct=1' >> o.myvac 2>&1
+    echo "Increase idle_pct to 100 to flush InnoDB buffer pool" >> o.myvac
+    $client "${mya[@]}" -e 'set global innodb_idle_flush_pct=100' >> o.myvac 2>&1
+    $client "${mya[@]}" -e 'show global variables' >> o.myvac.show.1 2>&1
+
+  elif [[ $e == "rocksdb" ]]; then
+    echo Enable flush memtable and L0 in 2 parts >> o.myvac
+    $client "${mya[@]}" -e "show engine rocksdb status\G" >& o.myvac.es1
+    echo "Flush memtable at $( date )" >> o.myvac
+    $client "${mya[@]}" -e 'set global rocksdb_force_flush_memtable_now=1' >> o.myvac 2>&1
+    sleep 20
+    echo "Flush memtable and lzero at $( date )" >> o.myvac
+    $client "${mya[@]}" -e "show engine rocksdb status\G" >& o.myvac.es2
+    $client "${mya[@]}" -e 'set global rocksdb_force_flush_memtable_and_lzero_now=1' >> o.myvac 2>&1
+  fi
+
+  now_secs=$( date +%s )
+  if [[ now_secs -lt done_secs ]]; then
+    diff_secs=$(( done_secs - now_secs ))
+    echo Sleep $diff_secs >> o.myvac
+    sleep $diff_secs
+  fi
+
+  for x in $( seq 1 $ntabs ); do
+    echo After load: wait for analyze $n >> o.myvac
+    wait ${apid[${x}]}
+  done
+  echo "Done waiting for analyze" >> o.myvac
+
+  if [[ $e == "innodb" ]]; then
+    echo "Reset max_dirty to $maxDirty and lwm to $maxDirtyLwm" >> o.myvac
+    $client "${mya[@]}" -e "set global innodb_max_dirty_pages_pct=$maxDirty" >> o.myvac 2>&1
+    $client "${mya[@]}" -e "set global innodb_max_dirty_pages_pct_lwm=$maxDirtyLwm" >> o.myvac 2>&1
+    echo "Reset idle_pct to $idlePct" >> o.myvac
+    $client "${mya[@]}" -e "set global innodb_idle_flush_pct=$idlePct" >> o.myvac 2>&1
+    $client "${mya[@]}" -e 'show global variables' >> o.myvac.show.2 2>&1
+  elif [[ $e == "rocksdb" ]]; then
+    $client "${mya[@]}" -e "show engine rocksdb status\G" >& o.myvac.es3
+  fi
+
+  now_secs=$( date +%s )
+  diff_secs=$(( now_secs - start_secs ))
+  echo "vac_my done after $diff_secs at $( date )" >> o.myvac
+}
+
+# if vac >=1 then "clean up" immediately prior to the read-write steps
+# no longer supported -- if > 1 then also clean up prior to each read-write step
+# "clean up" means
+#   if Postgres then do vacuum analyze and sleep for 1s per 1M rows
+#   if MySQL with InnoDB then do analyze, reconfigure to trigger writeback, sleep for 1s per 1M rows
+#   if MySQL with MyRocks then do analyze, reconfigure to trigger flush and compaction, sleep for 1s per 1M rows
 vac=1
 ns=3
 
@@ -97,10 +195,15 @@ ntabs=$dop
 if [[ $only1t == "yes" ]]; then ntabs=1; fi
 sfx=dop${ntabs}
 
-if [[ vac -ge 1 && $dbms == "postgres" ]] ; then
-  # Vaccum after load & index. Wait for vacuum to finish before starting read-write tests
-  vac_all 1
-  mv o.pgvac.* l.i1
+if [[ vac -ge 1 ]] ; then
+  if [[ $dbms == "postgres" ]] ; then
+    # Vaccum after load & index. Wait for vacuum to finish before starting read-write tests
+    vac_pg 
+    mv o.* l.i2
+  elif [[ $dbms == "mysql" ]]; then
+    vac_my 1
+    mv o.* l.i2
+  fi
 fi
 
 loop=1
@@ -109,12 +212,6 @@ farr=("$@")
 for ipsAndpk in "$@"; do
   ips=$( echo $ipsAndpk | tr ":" " " | awk '{ print $1 }' )
   querypk=$( echo $ipsAndpk | tr ":" " " | awk '{ print $2}' )
-
-  if [[ vac -gt 1 && $loop -gt 1 && $dbms == "postgres" ]] ; then
-    #Don't wait for vacuum to finish
-    vac_all 0
-    sleep 5
-  fi
 
   if [[ $querypk == "range" ]]; then
     # Run for querysecs seconds regardless of concurrency using range queries on the secondary indexes
