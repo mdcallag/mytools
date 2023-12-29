eac95e21af4809cf78b2027351344ddd602e234a
Mark Callaghan <mcallaghan@fb.com>
2023-12-29 10:14:24 -0800

    fix

--- bench/rx2/format_dbb_benchmark.sh
+++ bench/rx2/format_dbb_benchmark.sh
@@ -13,7 +13,7 @@ if [[ absval -eq 1 ]]; then
     cat $ifile | \
       awk '{ if (new == 1 && NF > 20 && $1 != "ops_sec" ) { printf "%s,", $1; tname=$(NF - 3) } ; if ($1 == "ops_sec") { new=1; printf "%s\n", tname }} END { printf "%s\n", tname } ' | \
       grep -v revrange\.t | grep -v overwritesome | \
-      awk '{ printf "%s", $NF; for (x=1 ; x < NF; x++ ) { printf ",%s", $x }; printf "\n" }'
+      awk -F ',' '{ printf "%s", $NF; for (x=1 ; x < NF; x++ ) { printf ",%s", $x }; printf "\n" }'
   fi
 else
   if [[ astsv -eq 1 ]]; then
@@ -26,7 +26,7 @@ else
     cat $ifile | \
       awk '{ if (new == 1 && NF > 20 && $1 != "ops_sec" ) { if (colno == 1) { printf "1.00,"; colval=$1 } else { printf "%.2f,", $1 / colval }; tname=$(NF - 3) ; colno = colno+1 } ; if ($1 == "ops_sec") { new=1; colno=1; printf "%s\n", tname }} END { printf "%s\n", tname } ' | \
       grep -v revrange\.t | grep -v overwritesome | \
-      awk '{ printf "%s", $NF; for (x=1 ; x < NF; x++ ) { printf ",%s", $x }; printf "\n" }'
+      awk -F ',' '{ printf "%s", $NF; for (x=1 ; x < NF; x++ ) { printf ",%s", $x }; printf "\n" }'
   fi
 fi
 
