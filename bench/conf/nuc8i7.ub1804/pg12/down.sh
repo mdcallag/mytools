c65dd4af855d9ab46330b06c04873b198a38774f
Mark Callaghan <mdcallag@gmail.com>
2024-02-01 18:17:32 +0000

    PG for 24-core

--- bench/conf/nuc8i7.ub1804/pg12/down.sh
+++ bench/conf/nuc8i7.ub1804/pg12/down.sh
@@ -1,8 +1,9 @@
+
 bdir=/data/m/pg
 
 echo "stop and remove files"
 bin/pg_ctl -D $bdir stop
+sleep 5
 rm -rf $bdir/*
 rm -f logfile; touch logfile
-sleep 3
 
--- bench/conf/nuc8i7.ub1804/pg16/down.sh
+++ bench/conf/nuc8i7.ub1804/pg16/down.sh
@@ -1,7 +1,9 @@
+
 bdir=/data/m/pg
 
 echo "stop and remove files"
 bin/pg_ctl -D $bdir stop
+sleep 5
 rm -rf $bdir/*
 rm -f logfile; touch logfile
 
