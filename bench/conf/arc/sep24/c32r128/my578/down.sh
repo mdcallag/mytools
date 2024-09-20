d1faf1557a81186eb911bcad12c973ec8d18ca96
Mark Callaghan <mdcallag@gmail.com>
2024-09-20 19:06:49 +0000

    more

---
+++ bench/conf/arc/sep24/c32r128/my578/down.sh
@@ -0,0 +1,22 @@
+echo Start shutdown at $( date )
+bin/mysqladmin -uroot -ppw shutdown
+echo shutdown finished at $( date )
+
+ssecs=$( date +%s )
+while ps aux | egrep "mariadb|mysqld" | grep -v grep ; do sleep 1; echo running; done
+esecs=$( date +%s )
+tsecs=$(( esecs - ssecs ))
+echo Total secs after shutdown is $tsecs
+cp /data/m/my/data/dell32.err .
+
+sleep 5
+
+rm -rf /data/m/my; mkdir -p /data/m/my
+mkdir -p /data/m/my/data
+mkdir -p /data/m/my/binlogs
+mkdir -p /data/m/my/txlogs
+
+rm -rf var
+mkdir var
+
+
