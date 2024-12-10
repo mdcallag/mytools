b6802cef4eab3f53cb30a93be3bc4d764ef3fc9a
Mark Callaghan <mdcallag@gmail.com>
2024-12-10 19:04:44 +0000

    more

--- bench/sysbench.lua/r2.sh
+++ bench/sysbench.lua/r2.sh
@@ -1,9 +1,14 @@
 t1=$1
 t2=$2
 tag=$3
+dop1=$4
+dop2=$5
+ntabs=$6
+nrows=$7
 
-bash r.sh 1 30000000 $t1 $t2 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 1
-mkdir $tag.dop1 ; mv x.* $tag.dop1
+bash r.sh $ntabs $nrows $t1 $t2 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 $dop1
+mkdir $tag.dop${dop1} ; mv x.* $tag.dop${dop1}
 
-bash r.sh 1 30000000 $t1 $t2 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 6
-mkdir $tag.dop6 ; mv x.* $tag.dop6
+bash r.sh $ntabs $nrows $t1 $t2 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 $dop2
+mkdir $tag.dop${dop2} ; mv x.* $tag.dop${dop2}
+
