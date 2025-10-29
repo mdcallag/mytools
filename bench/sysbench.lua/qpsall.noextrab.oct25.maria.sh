e1255fdefa5830cba033bbc6626eb0d10e061cbd
Mark Callaghan <mdcallag@gmail.com>
2025-10-29 15:03:03 +0000

    more

---
+++ bench/sysbench.lua/qpsall.noextrab.oct25.maria.sh
@@ -0,0 +1,103 @@
+cnf_suffix=$1
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.all $csv \
+x.ma100230_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100244_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100309_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100339_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100408_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100434_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100506_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100529_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100605_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100623_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100708_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100808_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100908_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101007_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101104_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101114_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma110408_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma110803_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_ahi1_${cnf_suffix}.pk1 \
+x.ma120101_mdev16168_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.best $csv \
+x.ma100230_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100244_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100309_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100339_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100408_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100434_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100506_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100529_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100605_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100623_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100708_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100808_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100908_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101007_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101104_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101114_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma110408_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma110803_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_ahi1_${cnf_suffix}.pk1 \
+x.ma120101_mdev16168_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.latest $csv \
+x.ma100230_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100244_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100339_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100434_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100529_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100623_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100708_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100808_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100908_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101007_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101114_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma110408_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma110803_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1002 $csv \
+x.ma100230_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100244_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1003 $csv \
+x.ma100309_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100339_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1004 $csv \
+x.ma100408_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100434_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1005 $csv \
+x.ma100506_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100529_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1006 $csv \
+x.ma100605_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma100623_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1011 $csv \
+x.ma101104_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+x.ma101114_rel_withdbg.z12a_${cnf_suffix}.pk1 \
+; done
+
+for csv in 0 1 ; do bash ../sum_qps_csv_nopre.sh ma.1201 $csv \
+x.ma120101_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+x.ma120101_rel_withdbg.z12b_ahi1_${cnf_suffix}.pk1 \
+x.ma120101_mdev16168_rel_withdbg.z12b_${cnf_suffix}.pk1 \
+; done
+
