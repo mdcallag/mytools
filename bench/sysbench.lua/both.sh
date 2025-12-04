f4b8cb5a0d5ebab32018ba6fd62d998662de7edd
Mark Callaghan <mdcallag@gmail.com>
2025-12-04 16:14:08 +0000

    fixes

--- bench/sysbench.lua/both.sh
+++ bench/sysbench.lua/both.sh
@@ -1,11 +1,10 @@
 tag=$1
-dev=$2
-p2=$3
 
-bash r.sh 1 50000000 600 900 $dev 1 1 1
+bash r.sh 1 50000000 600 900 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 1
 mkdir res.dop1.$tag
 mv x.* res.dop1.$tag
 
-bash r.sh 8 10000000 600 900 $dev 1 1 $p2
-mkdir res.dop${p2}.$tag
-mv x.* res.dop${p2}.$tag
+bash r.sh 4 20000000 300 600 $( df -h | awk '{ if ( $6 == "/data" ) { print $1 } }' | tr '/' ' ' | awk '{ print $2 }' ) 1 1 4
+mkdir res.dop4.$tag
+mv x.* res.dop4.$tag
+
